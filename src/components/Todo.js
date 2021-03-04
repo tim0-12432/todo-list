6e7a1024867789fe2bad8612453279580b2234ec
tim0-12432 <timo.kapellner@gmail.com>
2021-03-04 16:21:41 +0100

    added draggable items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Draggable } from 'react-beautiful-dnd';
 
 const Todo = ({ todo, todos, setTodos }) => {
     const deleteHandler = (e) => {
@@ -18,19 +19,36 @@ const Todo = ({ todo, todos, setTodos }) => {
             })
         );
     };
+    const getIndex = () => {
+        for (let index = 0; index < todos.length; index++) {
+            if (todo.id === todos[index].id) {
+                return index;
+            }
+        }
+    };
 
     return (
-        <div className="todo">
-            <li className={ `todo-item${todo.completed ? " completed" : ""}` }>
-                { todo.text }
-            </li>
-            <button onClick={ completeHandler } className="complete-btn">
-                <i className="fas fa-check" />
-            </button>
-            <button onClick={ deleteHandler } className="trash-btn">
-                <i className="fas fa-trash" />
-            </button>
-        </div>
+        <Draggable draggableId={ `draggable-${ todo.id }` } index={ getIndex() }>
+            {(provided, snapshot) => (
+                <div className="todo"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={{ ...provided.draggableProps.style, boxShadow: snapshot.isDraggingOver ? "0 0 30px rgba(0, 0, 0, .3)" : "" }}
+                >
+                    <i class="fas fa-equals" />
+                    <li className={ `todo-item${todo.completed ? " completed" : ""}` }>
+                        { todo.text }
+                    </li>
+                    <button onClick={ completeHandler } className="complete-btn">
+                        <i className="fas fa-check" />
+                    </button>
+                    <button onClick={ deleteHandler } className="trash-btn">
+                        <i className="fas fa-trash" />
+                    </button>
+                </div>
+            )}
+        </Draggable>
     );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,36 @@
 import React from "react";
 import Todo from "./Todo";
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const TodoList = ({ todos, setTodos }) => {
+    const onDragEnd = (param) => {
+        const sourceIndex = param.source.index;
+        const destinationIndex = param.destination?.index;
+        let copy = [...todos];
+        if (destinationIndex) {
+            copy.splice(destinationIndex, 0, copy.splice(sourceIndex, 1)[0])
+            setTodos(copy);
+        }
+    };
+
     return (
-        <div className="todo-container">
-            <ul className="todo-list">
-                { todos.map(todo => (
-                    <Todo todo={ todo } key={ todo.id } todos={ todos } setTodos={ setTodos } />
-                )) }
-            </ul>
-        </div>
+        <DragDropContext onDragEnd={ onDragEnd }>
+            <div className="todo-container">
+                <Droppable droppableId="droppable-todos" type="TODOS">
+                {(provided, snapshot) => (
+                    <ul className="todo-list"
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                    >
+                        { todos.map(todo => (
+                            <Todo todo={ todo } key={ todo.id } todos={ todos } setTodos={ setTodos } />
+                        )) }
+                        {provided.placeholder}
+                    </ul>
+                )}
+                </Droppable>
+            </div>
+        </DragDropContext>
     );
 }
 
